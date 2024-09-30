e5724b71c01f06a56e275d991202f7a271f8ec74
eliasjuk <eliasp.juk@hotmail.com>
2024-09-30 20:07:15 -0300

    paleta de cores

---
+++ src/utils/ColorPalette.java
@@ -0,0 +1,21 @@
+package utils;
+
+/**
+ *
+ * @author eliasjuk
+ * 
+ */
+
+public class ColorPalette {
+    
+    // Definindo as cores
+    public static final String GREEN = "\u001B[32m";       // Verde
+    public static final String YELLOW = "\u001B[33m";      // Amarelo
+    public static final String MAGENTA = "\u001B[35m";     // Magenta color
+    public static final String PINK = "\u001B[38;5;213m";  // Salmon Pink color
+    public static final String CYAN = "\u001B[36m";        // Cyan color
+    
+    
+    public static final String RESET = "\u001B[0m";        // Resetar cor
+
+}
